326f52257ce07ce30f16d40a9216c3100c364824
Irina Baeva <irina.baeva@bonial.com>
2023-03-08 23:23:51 +0100

    Fetch coordinates for hardcoded location

--- final/src/App.js
+++ final/src/App.js
@@ -1,5 +1,35 @@
+import { useState, useEffect } from 'react';
+
 const App = () => {
-  return <div>Start</div>;
+  const [coordinates, setCoordinates] = useState({});
+
+  const LOCATION = 'bratislava';
+
+  const GEO_DECODER_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${LOCATION}&limit=1&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`;
+
+  const getCoordinatesByLocation = async () => {
+    try {
+      const coordinatesData = await fetch(GEO_DECODER_URL);
+      const coordinatesDataJSON = await coordinatesData.json();
+      const { lon, lat, name } = coordinatesDataJSON[0];
+      setCoordinates({ lon, lat, name });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getCoordinatesByLocation();
+  }, []);
+
+  console.log(coordinates);
+
+  return (
+    <div>
+      <div>City: {LOCATION}</div>
+      <div>Air Quality index level: </div>
+    </div>
+  );
 };
 
 export default App;
