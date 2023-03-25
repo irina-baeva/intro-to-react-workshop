eb696c20d0e86f16fe3798c0137f4abe804da043
Irina Baeva <irina.baeva@bonial.com>
2023-03-25 21:28:50 +0100

    Update styles, update readme

--- final/src/index.js
+++ final/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 import App from './App';
 
-const theme = extendTheme();
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode('blue.800')(props),
+    },
+  }),
+};
+
+const theme = extendTheme({ styles });
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
